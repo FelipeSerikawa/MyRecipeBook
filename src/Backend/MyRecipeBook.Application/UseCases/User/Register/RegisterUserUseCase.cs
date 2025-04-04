997d1c778fda3b01e41c1c4e34d93b28826a5d8a
FelipeSerikawa <fe.sadao17@gmail.com>
2025-04-04 14:56:43 -0300

    feat: adding endpoint to register an user

---
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserUseCase.cs
@@ -0,0 +1,16 @@
+﻿using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses;
+
+namespace MyRecipeBook.Application.UseCases.User.Register
+{
+    public class RegisterUserUseCase
+    {
+        public ResponseRegisteredUserJson Execute(RequestRegisterUserJson request)
+        {
+            return new ResponseRegisteredUserJson
+            {
+                Name = request.Name
+            };
+        }
+    }
+}
