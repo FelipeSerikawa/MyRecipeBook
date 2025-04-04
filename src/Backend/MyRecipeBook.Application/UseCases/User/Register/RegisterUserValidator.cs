8a1e87d133decebbc91d1fac169ddb2899a78647
FelipeSerikawa <fe.sadao17@gmail.com>
2025-04-04 17:20:08 -0300

    feat: addind a culture middleware

--- src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserValidator.cs
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserValidator.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Exceptions;
 
 namespace MyRecipeBook.Application.UseCases.User.Register
 {
@@ -7,7 +8,7 @@ namespace MyRecipeBook.Application.UseCases.User.Register
     {
         public RegisterUserValidator()
         {
-            RuleFor(user => user.Name).NotEmpty();
+            RuleFor(user => user.Name).NotEmpty().WithMessage(ResourceMessagesException.NAME_EMPTY);
             RuleFor(user => user.Email).NotEmpty();
             RuleFor(user => user.Email).EmailAddress();
             RuleFor(user => user.Password.Length).GreaterThanOrEqualTo(6);
